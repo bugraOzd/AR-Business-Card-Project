1a44d15a9ae571fd66acc866641eea130f5c5d29
bugraOzd <bugraozdilek@gmail.com>
2021-01-07 15:28:04 +0300

    AR Business Card Project Final
    AR Business Card Project Final Version

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class CameraScript : MonoBehaviour
+{
+    private bool isVuforiaStarted = false;
+
+    void Start()
+    {
+        VuforiaARController vuforia = VuforiaARController.Instance;
+
+        if(vuforia != null)
+        {
+            vuforia.RegisterVuforiaStartedCallback(StartAfterVuforia);
+        }
+    }
+
+    private void StartAfterVuforia()
+    {
+        isVuforiaStarted = true;
+        SetAutoFocus();
+    }
+
+    private void OnApplicationPause(bool isPaused)
+    {
+        if(!isPaused)
+        {
+            if(isVuforiaStarted)
+            {
+                SetAutoFocus();
+            }
+        }
+    }
+
+    private void SetAutoFocus()
+    {
+        if (CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO))
+        {
+            Debug.Log("Autofocus is set!");
+        } 
+        else
+        {
+            Debug.Log("Device does not support autofocus!");
+        }
+    }
+}
---
+++ Assets/Scripts/ToggleCanvas.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleCanvas : MonoBehaviour
+{
+    public GameObject canvas;
+    public bool isEnabled = false;
+
+    public void toggleCanvas()
+    {
+        isEnabled = !isEnabled;
+        canvas.SetActive(isEnabled);
+    }
+
+}
